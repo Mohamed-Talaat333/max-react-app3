7e7c34dc204fbe33b31e55b9af50795f0d53702d
Mohamed Talaat <mohamedtalaat333@gmail.com>
2023-04-18 16:36:39 +0200

    latest

--- src/App.js
+++ src/App.js
@@ -1,10 +1,13 @@
 import React from 'react';
+import AddUser from './Users/AddUser';
+import UsersList from './Users/UsersList';
 
 
 function App() {
   return (
     <div>
-
+      <AddUser />
+      <UsersList />
     </div>
   );
 }
